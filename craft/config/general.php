e4f25f654d2a8c38afa96b3612efd4c217779cbb
yulu zhang <yulu@70kft.com>
2016-04-20 21:50:12 -0500

    adding website

---
+++ craft/config/general.php
@@ -0,0 +1,38 @@
+<?php
+
+return array(
+    '*' => array(
+        'omitScriptNameInUrls' => true,
+    ),
+
+    'localhost' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/public/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'fpma',
+        )
+    ),
+
+    // Treat *.vagrantshare.com the same as localhost
+    'vagrantshare' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/public/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'fpma',
+        )
+    ),
+    // '.com' => array(
+    //     'cooldownDuration' => 0,
+
+    //     'environmentVariables' => array(
+    //         'env' => 'prod',
+    //         'fileSystemPath' => '/opt/www/public',
+    //         'frontEndAssets' => '/dist/',
+    //         'siteName' => 'fpma',
+    //     )
+    // )
+);
